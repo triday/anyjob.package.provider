1b245ac0427702b86e8eeb48066bf1cf471d262d
pengbo.yang <obpt@sina.com>
2020-07-14 08:16:00 +0800

    add client sdk project

---
+++ src/AnyJob.Package.Impl.Rest.Client/RestClientPackageProvider.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using YS.Knife.Rest.Client;
+
+namespace AnyJob.Package.Impl.Rest.Client
+{
+    [YS.Knife.ServiceClass]
+    public class RestClientPackageProvider : ClientBase, IPackageProviderService
+    {
+        public RestClientPackageProvider(IHttpClientFactory httpClientFactory, IOptions<ApiServicesOptions> apiServicesOptions) : base(httpClientFactory, apiServicesOptions, "PackageService")
+        {
+        }
+
+        public Task<List<ActionInfo>> GetActions(string packageName, string version)
+        {
+            return this.SendHttp<List<ActionInfo>>(
+                    new RestApiInfo
+                    {
+                        Method = HttpMethod.Get,
+                        Path = "{packageName}/{version}/actions",
+                        Arguments = new List<RestArgument>
+                        {
+                            new RestArgument("packageName", ArgumentSource.FromRouter, packageName),
+                            new RestArgument("version", ArgumentSource.FromRouter, version),
+                        }
+                    }
+                );
+        }
+
+        public Task<PackageVersionInfo> GetLatestPackageVersion(string packageName)
+        {
+            return this.SendHttp<PackageVersionInfo>(
+                     new RestApiInfo
+                     {
+                         Method = HttpMethod.Get,
+                         Path = "{packageName}/versions/latest",
+                         Arguments = new List<RestArgument>
+                         {
+                            new RestArgument("packageName", ArgumentSource.FromRouter, packageName),
+                         }
+                     }
+                 );
+        }
+
+        public Task<List<PackageFileInfo>> GetPackageFiles(string packageName, string version)
+        {
+            return this.SendHttp<List<PackageFileInfo>>(
+                     new RestApiInfo
+                     {
+                         Method = HttpMethod.Get,
+                         Path = "{packageName}/{version}/files",
+                         Arguments = new List<RestArgument>
+                         {
+                            new RestArgument("packageName", ArgumentSource.FromRouter, packageName),
+                            new RestArgument("version", ArgumentSource.FromRouter, version),
+                         }
+                     }
+                 );
+        }
+
+        public Task<Dictionary<string, List<PackageVersionInfo>>> ListPackages()
+        {
+            return this.SendHttp<Dictionary<string, List<PackageVersionInfo>>>(
+                    new RestApiInfo
+                    {
+                        Method = HttpMethod.Get,
+                        Path = "all",
+                        Arguments = new List<RestArgument>
+                        {
+                        }
+                    }
+                );
+        }
+
+        public Task UploadPackage(PackageVersionContentInfo contentInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
